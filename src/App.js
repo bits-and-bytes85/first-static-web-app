f1a013e1974749b3fe8b1b145e130bf9e7d901c7
Shriya Gautam <57238679+bits-and-bytes85@users.noreply.github.com>
2023-05-13 13:39:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ function App() {
   var name = prompt("What is your name?");
   var wage = prompt("What is your hourly wage?");
   var hours = prompt("How many hours do you typically work per week?");
+  var bracket;
+  var yearly = wage*hurs*52;
+
+  if (yearly > 120000){
+    bracket = 'high';
+  } else if (yearly > 50000){
+    bracket = 'middle';
+  } else {
+    bracket = 'low'
+  }
 
   return <div>
     <title>
-      Let's learn to budget{name} !
+      Let's learn to budget {name} !
     </title>
     <query>
-      It looks like your yearly salary is about {wage*hours*52}. 
+      It looks like your yearly salary is about {yearly} dollars.
+      That places you in the {bracket} income bracket.
     </query>
     </div>;
 
 
 }
+
 
 export default App;
