e138d8613cfeb8bb336e7f11d8a68ac96d495bbe
Shriya Gautam <57238679+bits-and-bytes85@users.noreply.github.com>
2023-05-13 13:17:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  var name = prompt("What is your name?");
+  var wage = prompt("What is your hourly wage?");
+  var hours = prompt("How many hours do you typically work per week?");
+
+  return <div>
+    <title>
+      Let's learn to budget{name} !
+    </title>
+    <query>
+      It looks like your yearly salary is about {wage*hours*52}. 
+    </query>
+    </div>;
+
+
 }
 
 export default App;
